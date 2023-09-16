8e5acab367e00a136a56f89a8130d69367b15431
linh <linhltq28@gmail.com>
2023-09-16 16:29:22 +0700

    home done

---
+++ lib/view/user/home/components/welcome_content.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:iclean_mobile_app/utils/color_palette.dart';
+
+import '../../../../models/account.dart';
+import '../../../common/location/location_screen.dart';
+
+class welcome_content extends StatelessWidget {
+  const welcome_content({
+    super.key,
+    required this.userLogin,
+  });
+
+  final Account userLogin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+        image: DecorationImage(
+      image: AssetImage('assets/images/banner_1.jpg'), 
+      fit: BoxFit.cover, 
+    ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage:
+                        AssetImage(userLogin.profilePicture),
+                    radius: 24,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: SizedBox(
+                      width: 320,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                userLogin.fullname,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontFamily: 'Lato',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          LocationScreen()));
+                            },
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Icons.location_on,
+                                  size: 16,
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(width: 8),
+                                Expanded(
+                                  child: Text(
+                                    userLogin.address,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontFamily: 'Lato',
+                                      color: Colors.white,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                    
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 24),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20),
+              ),
+              border: Border.all(
+                color: ColorPalette.greyColor,
+              ),
+              color: Colors.white
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 8),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    "Khám phá và trải nghiệm các dịch vụ gia đình ngay hôm nay.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Lato',
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  
+                ),
+                const Divider(color: ColorPalette.greyColor),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(
+                          IconData(
+                            0xe0b2,
+                            fontFamily: 'MaterialIcons',
+                          ),
+                          size: 24,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          "120 000 VND",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'Lato',
+                            //fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //Divider(),
+                    Row(
+                      children: const [
+                        Icon(
+                          IconData(
+                            0xe0b2,
+                            fontFamily: 'MaterialIcons',
+                          ),
+                          size: 24,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          "120 000 VND",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'Lato',
+                            //fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/view/user/home/home_screen.dart
+++ lib/view/user/home/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:iclean_mobile_app/utils/color_palette.dart';
 import 'package:iclean_mobile_app/view/user/home/components/banner_slider.dart';
 
 import '../../../models/account.dart';
+import '../../../models/services.dart';
+import 'components/welcome_content.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -18,228 +20,134 @@ class HomeScreen extends StatelessWidget {
       address:
           "S102 Vinhomes Grand Park, Nguyễn Xiễn, P. Long Thạnh Mỹ, Tp. Thủ Đức");
 
+  List<Service> services = [
+    Service(id: 1, name: "Giặt thảm", icon: "assets/images/clean_tham.jpg"),
+    Service(
+        id: 2, name: "Vệ sinh kính", icon: "assets/images/clean_kinh.png"),
+    Service(id: 3, name: "Giặt ủi", icon: "assets/images/giat_ui.png"),
+    Service(
+        id: 4,
+        name: "Dọn nhà vệ sinh",
+        icon: "assets/images/don_nha_ve_sinh.png"),
+    Service(id: 5, name: "Nấu ăn", icon: "assets/images/nau_an.png"),
+    Service(id: 6, name: "Decor", icon: "assets/images/decor.png"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromRGBO(149, 117, 205, 1),
-                    Color.fromRGBO(103, 58, 183, 1),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //top
+              welcome_content(userLogin: userLogin),
+              
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0, left: 24),
+                child: Text(
+                  "What's news?",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Lato',
+                    fontSize: 18,
+                  ),
                 ),
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //slider
+              const Center(child: BannerSlider()),
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0, left: 24),
+                child: Text(
+                  "Dịch vụ",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Lato',
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: GridView.count(
+                    crossAxisCount: 3,
+                    //physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    childAspectRatio: (80 / 72),
                     children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundImage:
-                                AssetImage(userLogin.profilePicture),
-                            radius: 24,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8),
-                            child: SizedBox(
-                              width: 286,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        userLogin.fullname,
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          fontFamily: 'Lato',
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 8),
-                                  GestureDetector(
-                                    // onTap: () {
-                                    //   Navigator.push(
-                                    //       context,
-                                    //       MaterialPageRoute(
-                                    //           builder: (context) =>
-                                    //               LocationScreen()));
-                                    // },
-                                    child: Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.location_on,
-                                          size: 16,
-                                          color: Colors.white,
-                                        ),
-                                        const SizedBox(width: 8),
-                                        Expanded(
-                                          child: Text(
-                                            userLogin.address,
-                                            style: const TextStyle(
-                                              fontSize: 14,
-                                              fontFamily: 'Lato',
-                                              color: Colors.white,
-                                            ),
-                                            overflow: TextOverflow.ellipsis,
-                                            //maxLines: 3,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
+                      for (int i = 0; i < services.length; i++)
+                        Container(
+                          margin: const EdgeInsets.all(16),
+                          //padding: const EdgeInsets.all(0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: ColorPalette.greyColor,
                             ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(.1),
+                                blurRadius: 4.0,
+                                spreadRadius: .05,
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      GestureDetector(
-                        // onTap: () {
-                        //   Navigator.push(
-                        //       context,
-                        //       MaterialPageRoute(
-                        //           builder: (context) => MyBookmarkScreen()));
-                        // },
-                        child: const Icon(
-                          Icons.turned_in_not_outlined,
-                          size: 25,
-                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              InkWell(
+                                // onTap: () {
+                                //   Navigator.push(
+                                //     context,
+                                //     MaterialPageRoute(
+                                //       builder: (context) => EmployeeServiceScreen(
+                                //         service: services[i],
+                                //       ),
+                                //     ),
+                                //   );
+                                // },
+                                child: Container(
+                                  margin: const EdgeInsets.all(8),
+                                  child: Image.asset(
+                                    services[i].icon,
+                                    width: 40,
+                                    height: 40,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                              ),
+                              Flexible(
+                                child: LayoutBuilder(
+                                  builder: (BuildContext context,
+                                      BoxConstraints constraints) {
+              
+                                    return Text(
+                                      services[i].name,
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                        fontFamily: 'Lato',
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      maxLines: 2,
+                                      softWrap: true,
+                                      textAlign: TextAlign.center,
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
                     ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 24),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                      border: Border.all(
-                        color: ColorPalette.greyColor,
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(height: 8),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            "Khám phá và trải nghiệm các dịch vụ gia đình ngay hôm nay.",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'Lato',
-                              color: Colors.white,
-                              //fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.justify,
-                          ),
-                        ),
-                        const Divider(color: ColorPalette.greyColor),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Row(
-                              children: const [
-                                Icon(
-                                  IconData(
-                                    0xe0b2,
-                                    fontFamily: 'MaterialIcons',
-                                  ),
-                                  size: 24,
-                                  color: Colors.white,
-                                ),
-                                SizedBox(width: 8),
-                                Text(
-                                  "120 000 VND",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontFamily: 'Lato',
-                                    color: Colors.white,
-                                    //fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            //Divider(),
-                            Row(
-                              children: const [
-                                Icon(
-                                  IconData(
-                                    0xe0b2,
-                                    fontFamily: 'MaterialIcons',
-                                  ),
-                                  size: 24,
-                                  color: Colors.white,
-                                ),
-                                SizedBox(width: 8),
-                                Text(
-                                  "120 000 VND",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontFamily: 'Lato',
-                                    color: Colors.white,
-                                    //fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                      ],
-                    ),
-                  ),
-                ],
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 16.0, left: 24),
-              child: Text(
-                "What's news?",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Lato',
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            const Center(child: BannerSlider()),
-            const Padding(
-              padding: EdgeInsets.only(top: 16.0, left: 24),
-              child: Text(
-                "Dịch vụ",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Lato',
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
